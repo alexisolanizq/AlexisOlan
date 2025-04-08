a2db79da0babdb5479296a6856354d681ab8c659
Alexis Olán Izquierdo <alexisolan05@icloud.com>
2025-04-08 15:13:13 -0600

    Fixing files

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -3,20 +3,18 @@ export const projects = [
     id: 0,
     title: "HeyCenter",
     link: "https://hey-center.vercel.app/",
-    image:
-      "https://cvws.icloud-content.com/B/ARiGzYf5GVNyX6ZLSPRHjM1wh3hNARO47xqsstlAsu3t-i6zP5IxQs8I/chat.jpg?o=Ak4aDGj8HSW_o78PxkLtf9rMdglHHJO1KxHjgoT3PUsN&v=1&x=3&a=CAogXvJVF4MaPDVqahNRotPit5vb5rDVFE74Np_kKAJUqX8SbxDZv9e54TIY2Zyzu-EyIgEAUgRwh3hNWgQxQs8IaifhdXyNJyFwHCUT3Dqcq4Ul6z2E5z9-4X_YM6QbsFWH73F1kse4zN5yJx0zDTBHickPUMkZqcSQ8TEe_7Ca1PKjqJAotPPcDS0JaMPlU6qBXA&e=1744149728&fl=&r=b82e38d4-23b5-45e2-9ca1-250d57556064-1&k=gfn9nzWH1-Fi4Ek3xH-3OA&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=54&s=CgkUd7xRbBtbHxL_0BdaD8wx8hs&cd=i",
+    image: "https://llantastogo.com/images/chat.webp",
   },
   {
     id: 1,
     title: "Izea",
     link: "https://izea.vercel.app/",
-    image:
-      "https://cvws.icloud-content.com/B/ASTsSWKdNIQMgXOYvTBBlig2q8zTAfQCtaA_rpSjS0mWZ14iTw0rbVKZ/izea.jpg?o=AmMsj9ZHjXuMI-C1aFcMqKbJtV2P2vjorAOxlMYKVGWN&v=1&x=3&a=CAog55oi9Zz7Wne1ZH20refwdnA8kPb63JOXYFVU5yZzSbQSbxDWldO54TIY1vKuu-EyIgEAUgQ2q8zTWgQrbVKZaicNCciT8euXCrqq2QiI1M7RD-WjpjkIFuF7Y1xlIdGoPOzAhmK-O65yJ6qy6Gsut9-tzbNEhsmJBwXKc8748LQR24ur20fh64pdG4--lLLszQ&e=1744149657&fl=&r=5f6ae8ae-fddb-4ab5-94c2-1b5eac2beaf2-1&k=1AYnhvGlu14IEvEHUSpNtg&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=54&s=Qn4RmkiDMbMNLGSnTHIPQkgc5vc&cd=i",
+    image: "https://llantastogo.com/images/izea.webp",
   },
   {
     id: 2,
     title: "EIM",
     link: "https://eim.vercel.app/",
-    image: "https://cvws.icloud-content.com/B/AduuIKSX35FrzQjE_o5vxGFDmTkEAdcU8jFS3nxq6l4zlh2865UWlxF1/iem.jpg?o=AvZs7XL7IM0F0uxNFwf6esL263YlFC69y_nuSEVp3nFs&v=1&x=3&a=CAogpK3i6rCvmjckTqduIV1nlzO-WRXSZD5qyzQIIpDn8jISbxCa2tW54TIYmrexu-EyIgEAUgRDmTkEWgQWlxF1aietMhjBICfAVpVXBadroofskG1an4m41oF8xHBAelVJYSc_b7cRl7ByJ_VdnJQKb9A_oh9_lY1r0qAtaWGLrYToiG5VeZFTYpzXXokt-jTX1w&e=1744149699&fl=&r=a4b68aa9-dfdd-443b-9a2d-f196a4233413-1&k=S19IoTrMd225Lkt3r6ZYew&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=54&s=L3pXDt0PPPm1L59dRmLaO4F8zP8&cd=i",
+    image: "https://llantastogo.com/images/iem.webp",
   },
 ];
